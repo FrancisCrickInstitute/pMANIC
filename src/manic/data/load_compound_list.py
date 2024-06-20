f2b0a94c9e5c311330e358ebe9eae4dcda21b1d7
Jack Coutts <jackwgcoutts@proton.me>
2024-06-20 11:45:38 +0100

    Loading compound list functionality added.

--- src/manic/data/load_compound_list.py
+++ src/manic/data/load_compound_list.py
@@ -1,8 +1,10 @@
-from src.manic.data.compound_data_object import Compound
 import pandas as pd
+from src.manic.data.compound_data_object import Compound
 
 def load_compound_list(file_path):
-    df = pd.read_excel(file_path, engine='openpyxl')
+    # Read the Excel file
+    df = pd.read_excel(file_path)
+    # Create a list of Compound objects from the dataframe
     compounds = [
         Compound(row['name'], row['tR'], row['Mass0'], row['lOffset'], row['rOffset'])
         for _, row in df.iterrows()

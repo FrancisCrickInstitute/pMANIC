ab70d00677a75ea38686182872cb8db7b7f0bf9b
Jack Coutts <jackwgcoutts@proton.me>
2024-06-20 12:01:49 +0100

    Data loaded labels now change colour.

--- src/manic/utils/constants.py
+++ src/manic/utils/constants.py
@@ -2,5 +2,5 @@ from PySide6.QtGui import QColor
 
 APPLICATION_VERSION = "1.0.0"
 FONT = "Verdana"
-GREEN = QColor(215, 50, 50, 128)
-RED = QColor(102, 215, 102, 128)
+RED = QColor(215, 50, 50, 128)
+GREEN = QColor(102, 215, 102, 128)
